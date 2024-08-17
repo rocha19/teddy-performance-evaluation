c27b8b8ba656f55c486a82a0345be5e962dcb3c5
rocha19 <rochaarqueologia@outlook.com>
2024-08-16 21:08:54 -0300

    feat: add repository for dependency inverging with the pesistenia layer and add barrel pattern

---
+++ src/domain/repository/repository.ts
@@ -0,0 +1,8 @@
+export interface Repository<T> {
+	findByEmail(email: string): Promise<T>;
+	findById(id: string): Promise<T>;
+	findByIdAndUpdate(id: string, updatedT: Partial<T>): Promise<void>;
+	findByIdAndDelete(id: string): Promise<void>;
+	create(data: T, id?: string): Promise<void>;
+	findAll(): Promise<T[]>;
+}
