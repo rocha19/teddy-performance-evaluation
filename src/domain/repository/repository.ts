f869954720fe66c8268fb155b65cb424992a498c
rocha19 <rochaarqueologia@outlook.com>
2024-08-17 14:20:18 -0300

    fix: add a second optional parameter at update method to receive the user id and the short url id

--- src/domain/repository/repository.ts
+++ src/domain/repository/repository.ts
@@ -2,7 +2,7 @@ export interface Repository<T> {
 	findByEmail(email: string): Promise<T>;
 	findById(id: string): Promise<T>;
 	findByIdAndUpdate(id: string, updatedT: Partial<T>): Promise<void>;
-	findByIdAndDelete(id: string): Promise<void>;
+	findByIdAndDelete(id: string, nextId?: string): Promise<void>;
 	create(data: T, id?: string): Promise<void>;
 	findAll(): Promise<T[]>;
 }
