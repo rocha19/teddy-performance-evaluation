dead018f824946984306617916306fbcf9e79727
rocha19 <rochaarqueologia@outlook.com>
2024-08-18 10:31:27 -0300

    test: entities

--- test/domain/entity/user.spec.ts
+++ test/domain/entity/user.spec.ts
@@ -1,37 +1,51 @@
-import { User } from "@/domain";
+import { User } from "@/domain/entity";
 
 const email = "any@email.com";
 const password = "123456";
+const invalidEmail = "invalid-email";
+const invalidPassword = "123";
+const createdAt = "2024-08-18T12:34:56Z";
+const invalidCreatedAt = "invalid-date";
+const updatedAt = "2024-08-18T13:34:56Z";
+const invalidUpdatedAt = "invalid-date";
+const id = "749ce916-51c3-4df9-9cda-460ccc0f579c";
+const invalidId = "invalid-id";
 
 describe("Validation user", () => {
-	it("Should accept only valid email actor ", () => {
-		const sut = new User(email, password);
-		expect(sut).toBeTruthy();
+	it("Should create a User instance with valid attributes", () => {
+		const sut = new User(email, password, [], createdAt, updatedAt, id);
+		expect(sut).toBeInstanceOf(User);
 	});
-	// it('Should accept name actor even not being registered in the database ', () => {
-	//   const actor = 'João Luiz'
-	//   const sut = Name.validator(actor)
-	//   expect(sut).toBeTruthy()
-	// })
-	// it('Should not accept string empty ', () => {
-	//   const actor = ''
-	//   const sut = Name.validator(actor)
-	//   expect(sut).toBeFalsy()
-	// })
-	// it('Should accept only first name actor', () => {
-	//   const actor = 'Agatha'
-	//   const sut = Name.validator(actor)
-	//   expect(sut).toBeTruthy()
-	// })
-	// it('Should accept only second name actor', () => {
-	//   const actor = 'Silva'
-	//   const sut = Name.validator(actor)
-	//   expect(sut).toBeTruthy()
-	// })
-	// it('Should not accept actor langer than 40 syllables in first name actor', () => {
-	//   //duvida!
-	//   const actor = 'Rejan' + 'e'.repeat(70)
-	//   const sut = Name.validator(actor)
-	//   expect(sut).toBeFalsy()
-	// })
+
+	it("Should throw an error if the email is invalid", () => {
+		expect(() => new User(invalidEmail, password)).toThrow(
+			"Invalid email or password",
+		);
+	});
+
+	it("Should throw an error if the password is invalid", () => {
+		const sut = new User(email, password);
+		// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+		expect(() => (sut.password = invalidPassword)).toThrow(
+			"Invalid email or password",
+		);
+	});
+
+	it("Should throw an error if createdAt is invalid", () => {
+		expect(() => new User(email, password, [], invalidCreatedAt)).toThrow(
+			"Invalid createdAt",
+		);
+	});
+
+	it("Should throw an error if updatedAt is invalid", () => {
+		expect(
+			() => new User(email, password, [], createdAt, invalidUpdatedAt),
+		).toThrow("Invalid updatedAt");
+	});
+
+	it("Should throw an error if id is invalid", () => {
+		expect(
+			() => new User(email, password, [], createdAt, updatedAt, invalidId),
+		).toThrow("Invalid id");
+	});
 });
